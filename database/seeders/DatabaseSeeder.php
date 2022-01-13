6a8aefa5e27ba2d646d77b3777053bb22950ba91
Rodrigues Junior <juniorblindside@hotmail.com>
2022-01-13 20:59:54 -0300

    Criando alguns seeders no banco de dados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('units')->insert([
+            'name' => 'APT 100',
+            'id_owner' => '1'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 101',
+            'id_owner' => '1'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 200',
+            'id_owner' => '0'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 201',
+            'id_owner' => '0'
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Academia',
+            'cover' => 'gym.jpg',
+            'days' => '1,2,4,5',
+            'start_time' => '06:00:00',
+            'end_time' => '22:00:00'
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Piscina',
+            'cover' => 'pool.jpg',
+            'days' => '1,2,3,4,5',
+            'start_time' => '07:00:00',
+            'end_time' => '23:00:00'
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'churrasqueira',
+            'cover' => 'barbecue.jpg',
+            'days' => '4,5,6',
+            'start_time' => '07:00:00',
+            'end_time' => '23:00:00 '
+        ]);
+        DB::table('walls')->insert([
+            'title' => 'Titulo de aviso de teste',
+            'body' => 'vai corinthians',
+            'datecreated' => '2021-01-13 20:00:00'
+        ]);
+        DB::table('walls')->insert([
+            'title' => 'Alerta para todos',
+            'body' => 'treinamento primeiros socorros',
+            'datecreated' => '2021-01-13 22:00:00'
+        ]);
     }
 }
