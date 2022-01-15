d9482fbf47ddfb6f3b2a710e0e8ddc0c97959fc7
Rodrigues Junior <juniorblindside@hotmail.com>
2022-01-15 19:43:01 -0300

    Configurando o DocController, criando a leitura dos documentos

--- app/Http/Controllers/DocController.php
+++ app/Http/Controllers/DocController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doc;
 use Illuminate\Http\Request;
 
 class DocController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = ['error' => ''];
+
+        $docs = Doc::all();
+
+        foreach ($docs as $docKey => $docValue) {
+            $docs[$docKey]['fileurl'] = asset('storage/' . $docValue['fileurl']);
+        }
+
+        $array['list'] = $docs;
+
+        return $array;
+    }
 }
